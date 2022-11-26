1a7015a8a70817a9646dde3a97eb174da144817a
lordofwizard <advaitpandharpurkar619@gmail.com>
2022-11-26 17:06:03 +0530

    Working with only 1 window named HI, documentation [X] class funcs

--- src/game.cpp
+++ src/game.cpp
@@ -1,7 +1,5 @@
 #include "game.h"
 
-
-
 void Game::initializer(){
     /*
         This function Initializes the window and prints the error if
@@ -15,54 +13,69 @@ void Game::initializer(){
     }
     else{
         std::cout << "SDL initialized Successfully";
-        make_window("IDK, some project i guess");
     }
 }
 
 void Game::make_window(const char * window_name){
-    /* TODO Documentation */
+    /* This function makes a window object named window and put's it's memory
+        address inside the window pointer private variable we have declared
+    */
     window = SDL_CreateWindow(window_name,
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
         window_width,
         window_height,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN
+        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN // TO FORCE OPENGL as BackEnd in SDL
     );
-
     if (window == NULL) {
         std::cout << "Window Could not be initialized \nError " << SDL_GetError() << std::endl;
     }
     else{
+        // Creation of a surface object..!
         SDL_Surface * surface = make_surface();
+        SDL_FillRect(surface , NULL, SDL_MapRGB( surface->format, 0xFF, 0xFF, 0xFF ));
+        SDL_UpdateWindowSurface(window);
     }
 }
+
 void Game::event_handeling(){
+    // event Object... note it's a event OBJECT and not a pointer
     SDL_Event event;
     bool quit = false;
 
     while(!quit){
         while(SDL_PollEvent(&event)){
+            // event object's ENUM type is used..
             if(event.type == SDL_QUIT) quit = true;
         }
     }
 }
 
 SDL_Surface * Game::make_surface(){
+    // Returns a surface object with white background..! over the window which is private
+    // But in the scope of Game class
     SDL_Surface * surface = SDL_GetWindowSurface(window);
-    SDL_FillRect(surface , NULL, SDL_MapRGB( surface->format, 0xFF, 0xFF, 0xFF ));
-    SDL_UpdateWindowSurface(window);
     return surface;
 }
 
 
-
 Game::Game(const char * name){
+    /* CONSTRUCTOR
+        initialization of window + surface + event Handeling
+        Initializing SDL with OpenGL
+    */
+
     initializer();
     make_window(name);
     event_handeling();
 }
 
 Game::~Game(){
+    /* Destructor
+        Destorys the current window
+        Deletes all surface objects as well as
+        any data in memory is freed here
+    */
     SDL_DestroyWindow( window );
     SDL_Quit();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 
 int main(int argc, char ** argv){
-  std::cout << "HEllo i guess" << std::endl;
   Game game("Hi");
   return 0;
 }
